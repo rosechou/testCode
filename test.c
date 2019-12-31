15076de296610a617a55201f2870f3ef0eb98449
rosechou <jcchou100@gmail.com>
2019-12-31 10:02:00 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,16 +1,15 @@
 int MAIN()
 {
- int a,b,c,d,e,f,g;
+ int a,b,c,d,e;
   
  a = read();
  b = read();
  c = a + 2; 
  d = b + 4;
  e = a + 1;
- f = b;
- g = c*d - 1; 
+
  
- if (a || b || c && d && (e>1) || (f>1)&&(g<1) )
+ if (a || b || c && d && (e>1) )
   { 
     write("True \n");
   }
